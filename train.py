9c5633febb23ea90c93436939c92671bf73a5d0b
Kurumi5210 <jaychou1620@gmail.com>
2025-05-15 11:22:49 +0800

    finish run_validation

--- train.py
+++ train.py
@@ -34,6 +34,69 @@ def get_or_build_tokenizer(config, ds, lang):
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
 
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    encoder_output = model.encode(source, source_mask)
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+    
+        decode_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+        out = model.decode(encoder_output, source_mask, decoder_input, decode_mask)
+
+        prob = model.project(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)],dim=1)
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
+
+
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_state, write, num_examples=2):
+    model.eval()
+    count = 0
+
+    source_texts = []
+    excepted = []
+    predicted = []
+
+    console_with = 80
+
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+
+            assert encoder_input.size(0) == 1, "验证的时候的batch必须为1"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            excepted.append(target_text)
+            predicted.append(model_out_text)
+
+            print_msg('-'*console_with)
+            print_msg(f'SOURCE:{source_text}')
+            print_msg(f'TARGET:{target_text}')
+            print_msg(f'PREDICTED:{model_out_text}')
+
+            if count == num_examples:
+                break
+    
+    if write:
+        pass
+
 def get_dataset(config):
     ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
     # build tokenizers
@@ -91,9 +154,9 @@ def train_model(config):
     
     #训练循环
     for epoch in range(initial_epoch, config['num_epochs']):
-        model.train()
         batch_iterator = tqdm(train_dataloader, desc=f'Processing epoch {epoch:02d}')
         for batch in batch_iterator:
+            model.train()
             encoder_input = batch['encoder_input'].to(device)   #(batch, seq_len)
             decoder_input = batch['decoder_input'].to(device)   #(batch, seq_len)
             encoder_mask = batch['encoder_mask'].to(device)   #(batch, 1, 1, seq_len)
@@ -116,6 +179,7 @@ def train_model(config):
         optimizer.step()
         optimizer.zero_grad()
 
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
         global_step+=1
 
     model_filename = get_weight_file_path(config, f'{epoch:02d}')
